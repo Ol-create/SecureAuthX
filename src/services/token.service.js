4642878d800b74a73d9191fb59ab63e45e107085
Ol-create <oluola96@gmail.com>
2026-01-01 23:53:06 +0100

    feat: add JWT access and refresh token service

---
+++ src/services/token.service.js
@@ -0,0 +1,15 @@
+import jwt from "jsonwebtoken";
+
+export function generateAccessToken(user) {
+  return jwt.sign(
+    { id: user._id, role: user.role },
+    process.env.ACCESS_TOKEN_SECRET,
+    { expiresIn: "15m" }
+  );
+}
+
+export function generateRefreshToken(user) {
+  return jwt.sign({ id: user._id }, process.env.REFRESH_TOKEN_SECRET, {
+    expiresIn: "7d",
+  });
+}
