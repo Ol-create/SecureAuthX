8234945b0b5e2156ce9c6f470fc3181f33cfda04
Ol-create <oluola96@gmail.com>
2026-01-03 23:35:20 +0100

    feat: add refresh token endpoint

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,10 +1,14 @@
 import express from "express";
 import { register, login } from "../controllers/auth.controller.js";
 import { loginLimiter } from "../middleware/loginRateLimiter.js";
+import { refreshToken } from "../controllers/refresh.controller.js";
 
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", loginLimiter, login);
+
+
+router.post("/refresh", refreshToken);
 
 export default router;
