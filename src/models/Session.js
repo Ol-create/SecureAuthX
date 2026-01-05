1bf45b381fccf84b3382b53ad8b9377b5703f8fa
Ol-create <oluola96@gmail.com>
2026-01-05 20:12:46 +0100

    feat: add session model for device-based tracking

---
+++ src/models/Session.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const sessionSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    refreshTokenHash: {
+      type: String,
+      required: true,
+      select: false,
+    },
+    ipAddress: String,
+    userAgent: String,
+    device: String,
+    isValid: {
+      type: Boolean,
+      default: true,
+    },
+    lastUsedAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Session", sessionSchema);
