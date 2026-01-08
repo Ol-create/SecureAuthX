cef92ede87522ad42b8083ce3823034394600130
Ol-create <oluola96@gmail.com>
2026-01-08 23:16:04 +0100

    feat: add global logout to revoke all active sessions

--- src/routes/session.routes.js
+++ src/routes/session.routes.js
@@ -1,9 +1,11 @@
 import express from "express";
 import { authenticate } from "../middleware/authenticate.js";
 import { listSessions } from "../controllers/session.controller.js";
+import { logoutAllSessions } from "../controllers/session.controller.js";
 
 const router = express.Router();
 
 router.get("/", authenticate, listSessions);
+router.delete("/logout-all", authenticate, logoutAllSessions);
 
 export default router;
